3a8697d4f644ce2959988d88d36b35c412815f8e
Thomas Hallström <minimoto@hotmail.com>
2024-02-21 13:00:04 +0100

    Small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <div
-      className="flex items-center w-full bg-cover bg-center h-screen"
-      style={{ backgroundImage: 'url("image/background.avif")' }}
-    >
-      <div className="ml-24 text-6xl text-white hover:text-[#014a5d]">
-        Thomas Hallström
-      </div>
-      <div className="text-white ml-24 p-3 opacity-0 transition-opacity duration-300 hover:opacity-100">
-        webdesigner
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div
+				className="flex items-center w-full bg-cover bg-center h-screen"
+				style={{ backgroundImage: 'url("image/background.avif")' }}
+			>
+				<div className="ml-24 text-6xl text-white hover:text-[#014a5d]">
+					Thomas Hallström
+				</div>
+				<div className="text-white ml-24 p-3 opacity-0 transition-opacity duration-300 hover:opacity-100">
+					webdesigner
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
