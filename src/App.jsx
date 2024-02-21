0c78e3820ca25ff3439cbfc2c7d1691535c9a4b1
Thomas Hallström <minimoto@hotmail.com>
2024-02-21 13:08:48 +0100

    Only background

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import "./App.css";
+import styled from 'styled-components'
 
 function App() {
 	return (
 		<>
-			<div
-				className="flex items-center w-full bg-cover bg-center h-screen"
-				style={{ backgroundImage: 'url("image/background.avif")' }}
-			>
-				<div className="ml-24 text-6xl text-white hover:text-[#014a5d]">
-					Thomas Hallström
-				</div>
-				<div className="text-white ml-24 p-3 opacity-0 transition-opacity duration-300 hover:opacity-100">
-					webdesigner
-				</div>
-			</div>
+    <Background>
+      <img src="../image/background.avif" alt="" />
+    </Background>			
 		</>
 	);
 }
+
+const Background = styled.div`
+`
 
 export default App;
