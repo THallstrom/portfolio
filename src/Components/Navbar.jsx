5a508659754301ee0315ec4d2cbd5a3314bfc942
MrMinken <minimoto@hotmail.com>
2024-02-21 02:06:21 +0100

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+      <div className="bg-gray-400">
+        <ul className="flex">
+          <li>
+            <p className="text-white">Home</p>
+          </li>
+          <li>
+            <p className="text-white">About</p>
+          </li>
+          <li>
+            <p>Contact</p>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
