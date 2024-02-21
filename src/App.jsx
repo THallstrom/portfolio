9b381cd58920b56a4584544e3e4703d0d55bdd4a
Thomas Hallström <minimoto@hotmail.com>
2024-02-21 13:32:45 +0100

    Without Padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import "./App.css";
 // App.jsx
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Background = styled.div`
-  background-image: url('../image/background.avif');
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  height: 100vh;
-  /* Lägg till andra önskade stilar här */
+	background-image: url("../image/background.avif");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	height: 100vh;
+	/* Lägg till andra önskade stilar här */
 `;
 
 const Name = styled.h1`
-color: white;
-
+	color: white;
+	padding: 0;
+	margin: 0;
 `;
 
 function App() {
-  return (
-    <Background>
-      <Name>Thomas</Name>
-    </Background>
-  );
+	return (
+		<Background>
+			<Name>Thomas</Name>
+		</Background>
+	);
 }
 
 export default App;
