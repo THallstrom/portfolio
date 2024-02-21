78bc2de92596b51889d82dfd0c469b3f93f46bba
MrMinken <minimoto@hotmail.com>
2024-02-21 02:21:09 +0100

    small

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="flex items-center w-full bg-cover bg-center h-screen" style={{ backgroundImage: 'url("image/background.avif")'}}>
-      <div className="flex flex-col">
+    <div
+      className="flex items-center w-full bg-cover bg-center h-screen"
+      style={{ backgroundImage: 'url("image/background.avif")' }}
+    >
       <div className="ml-24 text-6xl text-white hover:text-[#014a5d]">
         Thomas Hallström
       </div>
-      <div className="text-white ml-24 p-3 opacity-0 transition-opacity duration-300 hover:opacity-100">webdesigner</div>
+      <div className="text-white ml-24 p-3 opacity-0 transition-opacity duration-300 hover:opacity-100">
+        webdesigner
       </div>
     </div>
   );
